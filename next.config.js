619530a448ef751de110dc99fc1efb814b28c4a8
DaviGleibson <davigleibson@gmail.com>
2025-11-18 15:25:18 -0300

    erros corri

--- next.config.js
+++ next.config.js
@@ -3,7 +3,30 @@
 const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
-    }
+    },
+    webpack: (config, { isServer }) => {
+        // Adiciona um loader para remover injeções do Tempo antes do processamento
+        // Este loader processa os arquivos antes do SWC
+        if (!isServer) {
+            config.module.rules.push({
+                test: /\.(ts|tsx|js|jsx)$/,
+                enforce: 'pre',
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'string-replace-loader',
+                        options: {
+                            search: /^import\s+["']\.\/inject-tempo-devtools[^"']*["'];\s*\n?/gm,
+                            replace: '',
+                            flags: 'g',
+                        },
+                    },
+                ],
+            });
+        }
+        
+        return config;
+    },
 };
 
 module.exports = nextConfig;
